5c721e87f8c2b06cc01756147026605c90759ae8
Glay-26 <844033898@qq.com>
2025-12-31 17:15:33 +0800

    添加频道列表API接口和类型定义

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.tsx'
+import {fetchChannelAPI} from '@/apis/list.ts'
+
+fetchChannelAPI().then(res=>{
+    console.log(res.data)
+})
 
 createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router}/>,
