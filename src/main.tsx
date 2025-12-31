8e4e33b9ceb9f078a545c466cfb6910cfef8435a
Glay-26 <844033898@qq.com>
2025-12-31 18:17:04 +0800

    实现首页频道列表展示功能

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.tsx'
 
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.tsx'
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,25 @@
+import './style.css'
+import { Tabs } from 'antd-mobile'
+import { fetchChannelAPI, type ChannelItem } from '@/apis/list'
+import { useState,useEffect } from 'react'
 const Home = () => {
-  return <div> this is Home </div>
+  const [channels, setChannels] = useState<ChannelItem[]>([])
+  useEffect(() => {
+    // 获取频道数据
+    const getChannels = async () => {
+      const res = await fetchChannelAPI()
+      setChannels(res.data.data.channels)
+    }
+    getChannels()
+  },[])
+  return <div className='tabContainer'>
+    <Tabs>
+      {channels.map((item)=>{
+        return <Tabs.Tab title={item.name} key={item.id}>
+          </Tabs.Tab>
+      })}
+        </Tabs>
+  </div>
 }
 
 export default Home
